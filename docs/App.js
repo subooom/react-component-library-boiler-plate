c42999f8b749d259d41809453db1be679436a1f5
subooom <subhamkharel12@gmail.com>
2019-07-10 22:50:50 +0545

    Basic setup

---
+++ docs/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { H1, H2, H3, H4, Button } from 'styledbyme'
+
+const App = () =>
+  <div>
+    <H1>Heading 1</H1>
+    <H2>Heading 2</H2>
+    <H3>Heading 3</H3>
+    <H4>Heading 4</H4>
+    <Button type="submit">Hello world</Button>
+  </div>
+
+export default App
---
+++ readme.md
@@ -0,0 +1,2 @@
+# React Component Library Boilerplate
+Use this as a start up point for your component library that you can push later to NPM.
